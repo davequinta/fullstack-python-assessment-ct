aa933144d3ac2304beda1ef8b4d4551f1c557ec5
Dave Quinta <david.quinta415@gmail.com>
2025-02-18 00:57:01 -0600

    feat: add web socket

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routes import product
+from app.routes import product, websockets
 
 app = FastAPI()
 
 app.include_router(product.router)
+app.include_router(websockets.router)
 
 @app.get("/")
 def read_root():
